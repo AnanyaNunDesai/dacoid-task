63c828965577d20f9d5638206f81fd6bb2b13a25
AnanyaNunDesai <ananyadesai290402@gmail.com>
2024-04-12 10:39:51 +0530

    Added upcoming training and schedule tracker

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      quicksand: ["Quicksand", "sans-serif"],
+    },
     extend: {
       fontFamily: {
         sans: ["Montserrat"],
-        Roboto: ["Roboto"]
+        Roboto: ["Roboto"],
       },
     },
   },
